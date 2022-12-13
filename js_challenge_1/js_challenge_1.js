72d186b9f3dd9a6f5b576e85fbf4742f554aec8c
dipakship <112948120+dipakship@users.noreply.github.com>
2022-12-13 13:30:21 -0500

    indent

--- js_challenge_1/js_challenge_1.js
+++ js_challenge_1/js_challenge_1.js
@@ -1,10 +1,12 @@
 //ask user their name
-let firstName=prompt ("What is your name?");
+let firstName=prompt ("What is your first name?");
+
+let lastName=prompt ("What is your last name?");
 
 // add second function here 
 
 // update the alert 
-alert("Hello " + firstName);
+alert("Hello " + firstName + lastName);
 
 // ask user how old they are & set age rules (hint: use an if/else statement here)
 
