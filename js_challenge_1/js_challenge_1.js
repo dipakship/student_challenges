dc121d0a63cd637b577ea201e29969a827f8d315
dipakship <112948120+dipakship@users.noreply.github.com>
2022-12-13 13:35:30 -0500

    added alerts

--- js_challenge_1/js_challenge_1.js
+++ js_challenge_1/js_challenge_1.js
@@ -1,13 +1,19 @@
 //ask user their name
 let firstName=prompt ("What is your first name?");
 
-let lastName=prompt ("What is your last name?");
-
 // add second function here 
+let lastName=prompt ("What is your last name?");
 
 // update the alert 
 alert("Hello " + firstName + lastName);
 
 // ask user how old they are & set age rules (hint: use an if/else statement here)
+let age=prompt("How old are you?")
 
+if(age < 14){
+    alert("You are too young to apply!")
+}
+else{
+    alert("Welcome!")
+}
 
