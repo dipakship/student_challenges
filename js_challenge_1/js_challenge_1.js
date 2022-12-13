fc509cbc289bdd44e800707f643e837d972fe269
dipakship <112948120+dipakship@users.noreply.github.com>
2022-12-13 13:47:27 -0500

    space in name

--- js_challenge_1/js_challenge_1.js
+++ js_challenge_1/js_challenge_1.js
@@ -5,7 +5,7 @@ let firstName=prompt ("What is your first name?");
 let lastName=prompt ("What is your last name?");
 
 // update the alert 
-alert("Hello " + firstName + lastName);
+alert("Hello " + firstName + " " + lastName);
 
 // ask user how old they are & set age rules (hint: use an if/else statement here)
 let age=prompt("How old are you?")
